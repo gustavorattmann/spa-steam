279c6dfad73a875c022eff6b8fd79fa5a081c584
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-10-29 12:15:29 -0300

    Adicionado card dentro da estrutura

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
 
+import Card from 'react-bootstrap/Card';
+
 import Header from '../partials/Header';
 import Footer from '../partials/Footer';
 
@@ -7,9 +9,17 @@ const Home = () => {
     return (
         <Fragment>
             <Header />
-            <div>
-                <h1>Teste</h1>
-            </div>
+            <main>
+                <Card style={{ width: '60%', background: 'rgba(255, 255, 255, .85)' }}>
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </main>
             <Footer />
         </Fragment>
     );
