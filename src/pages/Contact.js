df1398749fd30cf8c7bfeffad8d6418b18cbcb66
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-10-29 12:15:59 -0300

    Adicionado card dentro da estrutura e importado formulário

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,31 @@
-const Contact = () => {
+import { Fragment } from 'react';
+
+import Card from 'react-bootstrap/Card';
+
+import Header from '../partials/Header';
+import Footer from '../partials/Footer';
+
+import FormContact from '../elements/FormContact';
+
+const Home = () => {
     return (
-        <div>
-            <h1>Contact</h1>
-        </div>
+        <Fragment>
+            <Header />
+            <main className="d-flex flex-row align-items-center justify-content-center py-5">
+                <Card className="p-4" style={{ width: '60%', background: 'rgba(255, 255, 255, .85)' }}>
+                    <Card.Body>
+                        <Card.Title className="fs-1">Contato</Card.Title>
+                        <Card.Text>
+                            <p>Possui alguma dúvida ou deseja obter maiores informações sobre a maior empresa de distribuição de jogos digitais?<br />
+                            Preencha os campos abaixo para que possamos entrar em contato assim que possível.</p>
+                            <FormContact />
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </main>
+            <Footer />
+        </Fragment>
     );
 }
 
-export default Contact;
+export default Home;
