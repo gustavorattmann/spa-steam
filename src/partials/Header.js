036528029537fb99619a5a0841fcaf95efc54ee8
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-10-29 12:14:56 -0300

    Adicionado novos ícones, links e correção no tamanho da logo

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,27 +1,31 @@
+import { Link } from 'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faHeadset, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
-import logo from '../images/logo.svg';
+import logo from '../assets/images/logo_steam.svg';
 
 const Header = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">
+                <Navbar.Brand as={Link} to="/">
                     <img
                     src={logo}
                     className="d-inline-block align-top"
                     alt="Logo Steam"
+                    width="176"
+                    height="44"
                     />
                 </Navbar.Brand>
                 <Nav className="ms-auto">
-                    <Nav.Link href="#home"><FontAwesomeIcon icon={faHouse} /> Início</Nav.Link>
-                    <Nav.Link href="#features">Sobre</Nav.Link>
-                    <Nav.Link href="#pricing">Contato</Nav.Link>
+                    <Nav.Link as={Link} to="/"><FontAwesomeIcon icon={faHouse} /> Início</Nav.Link>
+                    <Nav.Link as={Link} to="/sobre"><FontAwesomeIcon icon={faCircleInfo} /> Sobre</Nav.Link>
+                    <Nav.Link as={Link} to="/contato"><FontAwesomeIcon icon={faHeadset} /> Contato</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
