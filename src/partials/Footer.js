3952bbf4a69dbeca681538d5f97a4ad3ba1618c4
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-10-29 12:14:17 -0300

    Correção no texto do rodapé e adicionado estilização

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer>
-            <p>Todos os direitos reservados ao Gustavo Rattmann.</p>
+        <footer className="text-bg-dark text-center py-4">
+            <p className="m-0">Copyright &copy; 2022 - Todos os direitos reservados ao Gustavo Rattmann.</p>
         </footer>
     );
 }
