d9b1578eb00fb39d6dbbb08129ba0715b455a320
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-10-31 03:09:51 -0300

    Componente responsável por carregar a tabela

--- src/elements/TableLayout.js
+++ src/elements/TableLayout.js
@@ -1,57 +1,49 @@
-import { useTable } from 'react-table';
-
 import Table from 'react-bootstrap/Table';
 
 const TableLayout = (props) => {
     let { columns, data } = props;
-
-    const tabela = useTable({ columns, data });
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = tabela
       
     return (
-        <Table variant="dark" striped bordered hover responsive {...getTableProps()}>
+        <Table variant="dark" striped bordered hover responsive className="align-middle" id="tabela-mais-jogados">
             <thead>
+                <tr>
+                {
+                    columns.map((column, i) => (
+                        <th key={i} className="text-center">
+                            { column }
+                        </th>
+                    ))
+                }
+                </tr>
+            </thead>
+            <tbody>
             {
-                headerGroups.map(headerGroup => (
-                    <tr className="align-middle text-center" { ...headerGroup.getHeaderGroupProps() }>
-                        {
-                            headerGroup.headers.map(column => (
-                            <th {...column.getHeaderProps()}>
-                                { column.render('Header') }
-                            </th>
-                            ))
-                        }
+                data.map((cell, i) => (
+                    cell === 'Nenhum registro encontrado!' ? 
+                    <tr key={i}>
+                        <td colSpan={7} className="text-center">
+                            Nenhum registro encontrado!
+                        </td>
+                    </tr>
+                    :
+                    <tr key={i}>
+                        <td className="align-middle text-center px-3">
+                            { cell.id }
+                        </td>
+                        <td className="align-middle px-3">
+                            { cell.game }
+                        </td>
+                        <td className="align-middle px-3">
+                            { cell.price }
+                        </td>
+                        <td className="align-middle text-end px-3">
+                            { cell.players }
+                        </td>
+                        <td className="align-middle text-end px-3">
+                            { cell.peak }
+                        </td>
                     </tr>
                 ))
-            }
-            </thead>
-            <tbody {...getTableBodyProps()}>
-            {
-                rows.map(row => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()}>
-                        {
-                            row.cells.map(cell => {
-                                console.log(cell.columns)
-
-                                return (
-                                    <td className="align-middle" {...cell.getCellProps()}>
-                                        { cell.render('Cell') }
-                                    </td>
-                                )
-                            })
-                        }
-                        </tr>
-                    );
-                })
             }
             </tbody>
         </Table>
