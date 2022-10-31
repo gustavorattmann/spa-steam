76e123f7f99f8261e6d8e6d059ba5fc830c4ef86
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2022-10-31 03:02:08 -0300

    Adicionado margin-top auto

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="text-bg-dark text-center py-4">
+        <footer className="text-bg-dark text-center py-4 mt-auto">
             <p className="m-0">Copyright &copy; 2022 - Todos os direitos reservados ao Gustavo Rattmann.</p>
         </footer>
     );
